0d150af4816adf0760cfac846e2355f94b772994
Brian <brian.sila1@student.moringaschool.com>
2025-03-05 13:48:25 +0300

    object-lab

---
+++ src/00-objectball.js
@@ -0,0 +1,16 @@
+const gameObject = {
+    home:{
+        teamName : "Afc Wimledon",
+        color: "blue",
+        players: "11",
+        coach: "John Doe",
+        team: "home",
+    },
+    away : {
+        teamName: "Manchester United",
+        color: "red",
+        players: "11",
+        coach: "John Doe",
+    },
+
+};
